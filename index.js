0db1c3b0904e70e36ea935a3a17b09b9059e070a
sergio g <ssergio.guerrero1@gmail.com>
2020-04-09 09:10:44 -0500

    initial commit by Sergio

---
+++ index.js
@@ -0,0 +1,20 @@
+// *************************************
+// author: Sergio Guerrero
+// hello world node app
+// *************************************
+
+// to run, open the cmd line and type
+// node index.js
+var http = require("http");
+
+http.createServer(function (request, response) {
+   
+   // response headers
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // and response message
+   response.end('Hello World');
+}).listen(8081);
+
+// log on the console
+console.log('Server running at http://127.0.0.1:8081/');
